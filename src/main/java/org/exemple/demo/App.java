769075a33c2ed97d5b46d16740e9aab34b132652
Dardouriwafa <36707598+Dardouriwafa@users.noreply.github.com>
2020-03-19 14:52:17 +0100

    Update App.java

--- src/main/java/org/exemple/demo/App.java
+++ src/main/java/org/exemple/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World wafa " );
+        System.out.println( "Hello World wafa dardouri " );
     }
 }
